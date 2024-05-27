180c70d65c4230e832738e03ae62299fa4f01c5a
LaSteChips <121933446+LaSteChips@users.noreply.github.com>
2024-05-27 11:58:34 +0200

    final commit
    version 1.0
    test main.js : ok
    test des pages ejs : ok
    communication avecc la base de données : ok

--- main.js
+++ main.js
@@ -1,98 +1,113 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const DataManager = require('./datamanager');
+require('dotenv').config(); // Ajoutez cette ligne en haut
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const DataManager = require("./datamanager");
+
 const app = express();
 const port = 3000;
 
-const db = new DataManager('localhost', 'root', '', 'account');
+const db = new DataManager("localhost", "root", "", "account");
 db.Connect();
 
-// Configurer le moteur de templates EJS
-app.set('views', path.join(__dirname, 'pages'));
-app.set('view engine', 'ejs');
-
-// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true,
+}));
 
-// pour accéder aux fichiers CSS
-app.use(express.static(path.join(__dirname, 'pages')));
+// Définir le dossier contenant les fichiers statiques (CSS, images, etc.)
+app.use(express.static(__dirname));
 
-// Page d'accueil (également la page de connexion)
+app.set('view engine', 'ejs');
+app.set('views', './pages');
+
+// Page de connexion
 app.get('/', (req, res) => {
-    res.render('login', { message: null });
-});
-
-// Page pour créer un compte
-app.get('/create', (req, res) => {
-    res.render('create', { message: null });
-});
-
-app.post('/create', (req, res) => {
-    const { username, password } = req.body;
-    const sql = 'INSERT INTO account_data (user, password) VALUES (?, ?)';
-    db.query(sql, [username, password], (err, result) => {
-        if (err) {
-            console.error(err);
-            res.render('create', { message: 'Erreur lors de la création du compte.' });
-            return;
-        }
-        res.render('create', { message: 'Compte créé avec succès.' });
-    });
-});
-
-// Page pour supprimer un compte
-app.get('/delete', (req, res) => {
-    const sql = 'SELECT user FROM account_data';
-    db.query(sql, (err, results) => {
-        if (err) {
-            console.error(err);
-            res.render('delete', { message: 'Erreur lors de la récupération des utilisateurs.', users: [], query: req.query });
-            return;
-        }
-        res.render('delete', { message: null, users: results, query: req.query });
-    });
-});
-
-app.post('/delete', (req, res) => {
-    const { username, password } = req.body;
-    const sql = 'DELETE FROM account_data WHERE user = ? AND password = ?';
-    db.query(sql, [username, password], (err, result) => {
-        if (err) {
-            console.error(err);
-            res.redirect('/delete?error=1');
-            return;
-        }
-        if (result.affectedRows > 0) {
-            res.redirect('/delete?success=1');
-        } else {
-            res.redirect('/delete?error=1');
-        }
-    });
-});
-
-// Page pour se connecter et télécharger un fichier GitHub
-app.get('/login', (req, res) => {
-    res.render('login', { message: null });
+  res.render('login');
 });
 
 app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const sql = 'SELECT * FROM account_data WHERE user = ? AND password = ?';
-    db.query(sql, [username, password], (err, results) => {
-        if (err) {
-            console.error(err);
-            res.render('login', { message: 'Erreur lors de la connexion.' });
-            return;
-        }
-        if (results.length > 0) {
-            res.redirect('https://github.com/LaSteChips/TSOP/archive/refs/heads/main.zip');
+  const { username, password } = req.body;
+  db.query("SELECT * FROM account_data WHERE username = ?", [username], (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      bcrypt.compare(password, results[0].password, (err, isMatch) => {
+        if (err) throw err;
+        if (isMatch) {
+          req.session.user = results[0];
+          res.redirect('https://github.com/LaSteChips/TSOP/archive/refs/heads/main.zip');
         } else {
-            res.render('login', { message: 'Identifiant ou mot de passe incorrect.' });
+          res.send('Mot de passe incorrect !');
         }
+      });
+    } else {
+      res.send('Utilisateur non trouvé !');
+    }
+  });
+});
+
+// Page de création de compte
+app.get('/signup', (req, res) => {
+    db.query("SELECT * FROM account_data", (err, users) => {
+      if (err) throw err;
+      res.render('signup', { users: users });
     });
 });
 
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) throw err;
+    db.query("INSERT INTO account_data (username, password) VALUES (?, ?)", [username, hash], (err, results) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
+  });
+});
+
+// Page de suppression de compte
+app.get('/delete', (req, res) => {
+    db.query("SELECT * FROM account_data", (err, users) => {
+        if (err) throw err;
+    res.render('delete', { users: users });
+    });
+});
+
+// Page de suppression de compte
+app.post('/delete', (req, res) => {
+    const { username, password } = req.body;
+    db.query("SELECT * FROM account_data WHERE username = ?", [username], (err, results) => {
+      if (err) throw err;
+      if (results.length > 0) {
+        bcrypt.compare(password, results[0].password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            db.query("DELETE FROM account_data WHERE username = ?", [username], (err, results) => {
+              if (err) throw err;
+              res.redirect('/signup'); // Redirection vers la page de création de compte
+            });
+          } else {
+            res.send('Mot de passe incorrect !');
+          }
+        });
+      } else {
+        res.send('Utilisateur non trouvé !');
+      }
+    });
+  });  
+
+app.get('/welcome', (req, res) => {
+  if (req.session.user) {
+    res.send(`Bienvenue ${req.session.user.username}`);
+  } else {
+    res.redirect('/');
+  }
+});
+
 app.listen(port, () => {
-    console.log(`Serveur démarré sur http://localhost:${port}`);
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
 });
